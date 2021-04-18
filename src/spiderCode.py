648ac09e66f6acc173b20f812399884420d6b208
Yadhu Philip <yadhuphilip@gmail.com>
2021-04-18 21:39:38 +0530

    CRAWLER DANGEROUS

--- src/spiderCode.py
+++ src/spiderCode.py
@@ -3,40 +3,65 @@ from bs4 import BeautifulSoup
 
 
 def urlValidator(url):
-
+    #print("debug")
     if any(word in url for word in URLVALIDSTRINGS):
         return True
     return False   
 
+def prepareUrlForFile(cnt, newUrl):
+
+    preparedData = str(cnt)+ " " + newUrl + " \n"
+    return preparedData
 
 ##CONSTANTS
-ENTRY_URL = "https://www.digitalocean.com/community/tutorials/how-to-work-with-web-data-using-requests-and-beautiful-soup-with-python-3"
+LIMIT = 4000
+ENTRY_URL = "https://en.wikipedia.org/wiki/Launch_Vehicle_Digital_Computer" 
+#"https://www.digitalocean.com/community/tutorials/how-to-work-with-web-data-using-requests-and-beautiful-soup-with-python-3"
 URLVALIDSTRINGS = ["http", "https", ".com", ".in", ".org", ".co", ".gov"]
+FILE_NAME = "urlDB.txt"
+UNIQUE_SET = set()
+UNIQUENESS = 1
 
 
 ######
 world = []
-count = 0
+count = 1
 ######
 
-while ENTRY_URL and count<=1000:
+fPtr = open(FILE_NAME,'w')
+while ENTRY_URL and count <= LIMIT:
+    flag = 1
+
     
     response = requests.get(ENTRY_URL)
     if response.status_code == 200:
-
         content = BeautifulSoup(response.text, 'html.parser')
         #(content.prettify())
         list_of_A = content.findAll('a', href=True)
 
         for each in list_of_A:
-            # print(each['href'] ,"\n")
-            if( urlValidator( each['href'])):
-                #print(each['href'], urlValidator( each['href'] ),"\n")
-                world.append(each['href']) 
-                count+=1
-    if world is not None:
-        ENTRY_URL = world.pop(0)
+            newUrl = each['href']
+            if(count >0 and count % UNIQUENESS==0 ):
+                UNIQUE_SET.clear()
+            if( newUrl in UNIQUE_SET):
+                flag = 0
+            else:
+                UNIQUE_SET.add(newUrl)
 
+            if( urlValidator(newUrl) and flag):
+
+                world.append(newUrl) 
+                preparedStr = prepareUrlForFile(count, newUrl)
+                count+=1
+                if(count>0 and count%15==0):
+                    print("###-- " , count , "--###")
+                fPtr.write(preparedStr)
+    if len(world) > 0:
+        ENTRY_URL = world.pop(0)
+    else:
+        break
+        
+fPtr.close()
 print(count)
 
 
